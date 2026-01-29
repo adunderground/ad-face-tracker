51614d3804ef7ba07d28058d86539096a9733ee1
Arkady Dolina <55853692+adunderground@users.noreply.github.com>
2026-01-29 15:50:36 -0500

    add four faces

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import GazeTracker from '../hooks/GazeTracker'
-import './App.css'
+import React from 'react';
+import GazeTracker from '../hooks/GazeTracker';
+import './App.css';
 
 function App() {
   return (
     <div className="app">
       <div className="face-tracker-container">
-        <GazeTracker />
+        <GazeTracker className="face-1" />
+        <GazeTracker className="face-2" />
+        <GazeTracker className="face-3" />
+        <GazeTracker className="face-4" />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
