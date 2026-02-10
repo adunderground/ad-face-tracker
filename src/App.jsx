ab969c47208bc73e91fd80c81265d764d5aa9cd8
Arkady Dolina <55853692+adunderground@users.noreply.github.com>
2026-02-10 17:31:45 -0500

    ship it

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import GazeTracker from '../hooks/GazeTracker';
-import Header from './components/Header'
+import Header from './components/Header';
+// import Footer from './components/Footer'
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <div className="face-tracker-container">
         <GazeTracker />
       </div>
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
   return (
-    <div className='header-container'>
-      <h1>Hello, I'm Arkady Dolina. </h1>
-      Senior Software Engineer based in Washington
-      D.C.
+    <div className="header-container">
+      <div className="bio">
+        <h1>
+          Hello, I'm <span className="name">Arkady Dolina</span>
+        </h1>
+        Senior Software Engineer based in Washington D.C.
+      </div>
+      <div className="bio">
+        I provide software solutions to complex business problems, <br />
+        create eye-catching functional sites, lead teams to the finish line, win
+        UX Hackathons, and confidently surf both the web and <br />
+        (much less confidently) the waves.
+      </div>
+      <div className="button">
+        <a href="/assets/ArkadyDolinaResume.pdf" noref noopener target="_blank">
+          View My Resume
+        </a>
+      </div>
     </div>
   );
 };
