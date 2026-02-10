becc24697dfec54185845595b6a0ccb8cf50b024
Arkady Dolina <55853692+adunderground@users.noreply.github.com>
2026-02-10 15:58:42 -0500

    resume branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import GazeTracker from '../hooks/GazeTracker'
-import './App.css'
+import React from 'react';
+import GazeTracker from '../hooks/GazeTracker';
+import Header from './components/Header'
+import './App.css';
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <div className="face-tracker-container">
         <GazeTracker />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='header-container'>
+      <h1>Hello, I'm Arkady Dolina. </h1>
+      Senior Software Engineer based in Washington
+      D.C.
+    </div>
+  );
+};
+
+export default Header;
